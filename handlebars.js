871d295e843172906a79e471dcbcea669e2f02ec
Karin <karin.j.alvarez@gmail.com>
2014-02-10 16:14:08 -0700

    Final additions to buttons

--- handlebars.js
+++ handlebars.js
@@ -23,7 +23,10 @@ $('.addButton').on('click', function() { /*Is there a better way to do this or i
 	});
 
 $(document).on('click', '.deleteButton', function () {
-		alert('Hey!');
+	($(this).closest('li').hide());
+	$('.deleteButton').closest.hide();
+	;
+
 	});
 
 });
