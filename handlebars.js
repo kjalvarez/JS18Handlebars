a15994e01004c4b676462641fc7b2839a01b71e0
Karin <karin.j.alvarez@gmail.com>
2014-02-10 10:58:29 -0700

    Initial commit

---
+++ handlebars.js
@@ -0,0 +1,13 @@
+$(document).on('ready', function() {
+
+var source = $('#videoGames').html();
+
+var template = Handlebars.compile(source);
+
+for (var i=0; i<productsData.productsList.length; i++) {
+	template(i);
+	$('#product-container').append(template(i));
+
+};
+
+});
